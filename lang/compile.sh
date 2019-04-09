8adb144b5d50b6450f69b8f798e77b7517349b2e
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2019-04-10 00:17:03 +0200

    Update script files

--- lang/compile.sh
+++ lang/compile.sh
@@ -3,5 +3,5 @@ rm *.hs
 rm *.x
 rm *.y
 bnfc -m --haskell Varg.cf && bnfc --latex Varg.cf && happy -i ParVarg.y && alex LexVarg.x
-cp AbsVarg.hs LexVarg.hs ParVarg.hs LayoutVarg.hs PrintVarg.hs ../Zad2/src
-cp Varg.tex ../../../Aplikacje/Overleaf/Varg
+cp AbsVarg.hs LexVarg.hs ParVarg.hs LayoutVarg.hs PrintVarg.hs ../src
+cp Varg.tex ../../../../Aplikacje/Overleaf/Varg
